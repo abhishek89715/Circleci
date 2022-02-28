ac0c479b3b169c4bae71d20f467755b5bb4c4a95
Abhishek <Abhishek@C40LAP-12502.india.rsystems.com>
2022-02-28 15:54:09 +0530

    main test file is updated

--- main-test.py
+++ main-test.py
@@ -3,7 +3,7 @@ from main import Add
 
 def TestAdd():
         assert Add(5,5) == 10
-        print("Add Function works correctly")
+        print("Add Function works correctly ....")
 
 if __name__ == '__main__':
         TestAdd()
